19b8ca636b92966e7bf693bd7006588f067501de
webmehedi99 <webmehedi99@gmail.com>
2024-09-23 08:35:14 +0600

    adding home page to the login button

--- js/login.js
+++ js/login.js
@@ -5,5 +5,16 @@ document.getElementById('button-login')
           event.preventDefault();
 
           const phoneNumber = document.getElementById('phone-number').value;
-          console.log(phoneNumber);
+          const pinNumber = document.getElementById('pin-number').value;
+          console.log(phoneNumber, pinNumber);
+
+        //   this is temporary. me should not do like this
+
+        if (phoneNumber === '5' && pinNumber === '1234'){
+            console.log('You are logged in');
+            window.location.href = '/home.html';
+        }
+        else{
+            alert('Wrong Phone Number or Pin number');
+        }
     })

b71d72640a50f898397640e1cf75c9616a578741
webmehedi99 <webmehedi99@gmail.com>
2024-09-23 09:10:00 +0600

    added input add money and input pin number

--- js/home.js
+++ js/home.js
@@ -1,1 +1,7 @@
-console.log('Home ');
+// step 01
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+    event.preventDefault();
+    const inputAddMoney = document.getElementById('input-add-money').value ;
+    const inputPinNumber = document.getElementById('input-pin-number').value;
+    console.log(inputAddMoney, inputPinNumber);
+})
